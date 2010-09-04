feca6e01271852d038ce1a3f125fa5ab95fea0f7
Marco Scholl <develop@marco-scholl.de>
2010-09-04 23:30:23 +0200

    activerecord support now in easy_captcha

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 end
-# 
-# # Devise Hack
-# Devise::RegistrationsController.class_eval do
-#   def build_resource(hash=nil)
-#     super(hash)
-#     self.resource.captcha_verification = session[:captcha]
-#   end
-# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,11 @@
 class User < ActiveRecord::Base
+  acts_as_easy_captcha
+  validate :valid_captcha?, :on => :create
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  attr_writer :captcha, :captcha_verification
-  
-  def captcha
-    ""
-  end
-  
-  validate :valid_captcha?
-  
-  def valid_captcha?
-    Rails.logger.debug "@captcha #{@captcha}"
-    Rails.logger.debug "@captcha_verification #{@captcha_verification}"
-    errors.add(:captcha, :captcha_invalid) unless @captcha.to_s.upcase == @captcha_verification.to_s.upcase
-  end
   
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :captcha
