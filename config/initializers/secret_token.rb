37472b2086db9aeae52929b6cbfd29b371f97b65
Marco Scholl <develop@marco-scholl.de>
2010-09-04 19:40:45 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyCaptchaSample::Application.config.secret_token = '0c30e952b43433f05fba7f71c3a7be95f6b56d6472f145c966f8ce6eb0a75015eeb1f8455ecc8a87ef734f72b423169663abb91e126bcf440e579a8c8d3ca2b1'
