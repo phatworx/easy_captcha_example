68abdefd4192e930874d6a7479a9d5a1f466ee7e
Marco Scholl <develop@marco-scholl.de>
2010-09-04 22:25:21 +0200

    new example: simple form

--- app/controllers/devise/registrations_controllers2.rb
+++
@@ -1,6 +0,0 @@
-require 'app/controllers/devise/registrations_controller.rb'
-
-class Devise::RegistrationsController < ApplicationController
-  def new
-  end
-end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,14 @@ class PagesController < ApplicationController
   def index
     
   end
+  
+  def simple_form
+    if request.post?
+      if valid_captcha? params[:captcha]
+        flash[:notice] = "valid captcha"
+      else
+        flash[:alert] = "invalid captcha"
+      end
+    end
+  end
 end
